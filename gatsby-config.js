bde498819ff6ab19565eecbb503cedb846e9b8f7
Ekwuno Obinna <obinnacodes@gmail.com>
2020-03-14 02:09:46 +0100

    testing tutorial for using multiple themes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,47 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `Your Site Title`,
+    description: `A description for your blazing fast site, using multiple themes!`,
+    author: `Your name`,
+    // highlight-start
+    menuLinks: [ 
+      {
+        name: `Blog`,
+        url: `/`,
+      },
+      {
+        name: `Notes`,
+        url: `/notes`,
+      },
+    ],
+    social: [
+      {
+        name: `Twitter`,
+        url: `https://twitter.com/gatsbyjs`,
+      },
+      {
+        name: `GitHub`,
+        url: `https://github.com/gatsbyjs`,
+      },
+    ], 
+     // highlight-end
+  },
+  plugins: [
+    `@pauliescanlon/gatsby-mdx-embed`,
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {
+        // basePath defaults to `/`
+        basePath: `/`,
+      },
+    },
+    {
+      resolve: `gatsby-theme-notes`,
+      options: {
+        // basePath defaults to `/`
+        basePath: `/notes`,
+      },
+    },
+    
+  ],
 }
